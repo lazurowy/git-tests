8e5b1b8fe9b7d9c1629ed36ca642fd3a08641c7f
lazurowy <lazurowy.mlecz@gmail.com>
2017-07-12 08:14:51 +0200

    change

--- src/main/resources/app.js
+++ src/main/resources/app.js
@@ -6,6 +6,7 @@ var server = http.createServer(function (request, response) {
   console.log("loaded!");
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
+  console.log("Request ended.");
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
